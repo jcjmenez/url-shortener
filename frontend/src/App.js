e982860986a276b74fb9b9782fc041349a4dd008
jcjmenez <jcjmenez@gmail.com>
2021-08-10 19:03:29 +0200

    Added frontpage and structured project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [urlInput, setUrlInput] = useState('');
+  const [urls, setUrls] = useState([]);
+
+  const handleUrlChange = (event) => {
+    event.preventDefault();
+    setUrlInput(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>URL Shortener</h1>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        const ids = urls.map((url) => url.id);
+        const maxId = ids.length > 0
+          ? Math.max(...ids) + 1
+          : 0;
+        const urlToAdd = { id: maxId || 0, longUrl: urlInput };
+        setUrls([...urls, urlToAdd]);
+      }}
+      >
+        <input type="text" name="url" onChange={handleUrlChange} />
+        <input type="submit" value="Short" />
+      </form>
+      <hr />
+
+      <div>
+        <h2>Shorted urls</h2>
+        <ul>
+          {
+          urls.length > 0
+            ? urls.map((url) => (
+              <li key={url.id}>{url.longUrl}</li>
+            ))
+            : 'Input a url to short'
+          }
+        </ul>
+      </div>
     </div>
   );
 }
